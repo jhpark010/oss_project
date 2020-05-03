c959c09546823dd7eb970e28f05e6efa7ed03efe
jhpark010 <63222190+jhpark010@users.noreply.github.com>
2020-05-03 19:36:05 +0900

    Revert "calculate source"

--- hello.c
+++ hello.c
@@ -1,15 +1,8 @@
 #include<stdio.h>
 
 int main(){
-	
-	int a = 10;
-	int b = 20;
-
 	printf("Hello Github!");
-	printf("----------------------");
-	printf("간단한 연산 프로그램");
-	printf("a + b = %d",a+b);
-
+	printf("추가된 내용입니다.");
 
 	return 0;
 }
